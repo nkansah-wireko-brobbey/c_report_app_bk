17d5746c8da553a33b3a4b9a29476c1e6c23bc38
nkansah-wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2025-01-18 20:50:38 -0600

    Add data models

---
+++ reporting/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+
+class Employee(models.Model):
+    employee_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+
+    name = models.CharField(
+        max_length=225, 
+        blank=False, 
+        null=False
+    )
+
+    def __self__(self):
+        return self.name
+
+class Phone(models.Model):
+    phone_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+    brand = models.CharField(
+        max_length=500
+    )
+    model = models.CharField(max_length=80)
+    price = models.DecimalField(max_digits=12,decimal_places=2)
+    quantity = models.PositiveBigIntegerField()
+
+class Shipping(models.Model):
+    shipping_id  = models.UUIDField(
+        primary_key=True,
+        default = uuid.uuid4,
+        blank= False,
+        null  = False
+    )
+    phone  = models.ForeignKey(Phone, on_delete=models.PROTECT)
+    shipped_date = models.DateField()
+    quantity = models.IntegerField()
+    destination = models.CharField(max_length=100)
+
+class Receiving(models.Model):
+    receiving_id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        blank=False
+    )
+    phone = models.ForeignKey(Phone, on_delete= models.PROTECT)
+
